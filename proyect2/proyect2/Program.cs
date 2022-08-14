10e7577027b1ddda34f9686c579d65389e710f6b
FrankUgb <111204154+FrankUgb@users.noreply.github.com>
2022-08-14 17:01:50 -0600

    Proyecto2

---
+++ proyect2/proyect2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace proyect2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el numero de repetciones");
+            double repe = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= repe; i++) {
+
+                Console.WriteLine(i);
+
+                Console.WriteLine("Escriba la altura (H) del triangulo");
+                double altura = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escriba el lado (A) del triangulo");
+                double A = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escriba el lado (Base) del triangulo");
+                double B = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escriba el lado (C) del triangulo");
+                double C = int.Parse(Console.ReadLine());
+
+                double Area = (B * altura) / 2; 
+                double Perimetro = A + B + C;
+
+                Console.Write($"El area del trigulo es de: {Area}cm2 y \n");
+                Console.Write($"el perimetro del triangulo es de: {Perimetro}cm2 \n");
+                Console.ReadKey();
+            }
+
+        }
+
+            
+    }
+}
---
+++ proyect3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace proyect3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Escriba 0, si desea detener el programa");
+            int stop = int.Parse(Console.ReadLine());
+
+            do {
+                Console.WriteLine("Escriba la altura (H) del cilindro");
+                double altura = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escriba el radio (R) del cilindro");
+                double R = double.Parse(Console.ReadLine());
+
+                double R2 = (R + altura);
+                double Area = (3.14 * 2) * R2;
+                double Volumen = 3.14 * (R * 2) * altura;
+
+                Console.Write($"El area del cilindro es de: {Area} cm2 y \n");
+                Console.Write($"el perimetro del cilindro es de: {Volumen} cm2 ");
+
+                Console.ReadKey();
+
+            } while (stop == 0);
+            Console.ReadKey();
+        }
+    }
+}
